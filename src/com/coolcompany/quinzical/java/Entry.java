b443734187840b43ed3a9b631b437d7197046f88
Fraser McCallum <fraser@themccallums.net.nz>
2020-09-10 18:36:03 +1200

    Created directory structure

---
+++ src/com/coolcompany/quinzical/java/Entry.java
@@ -0,0 +1,13 @@
+package com.coolcompany.quinzical.java;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Entry extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Jeopardy");
+        stage.show();
+    }
+}
---
+++ src/com/coolcompany/quinzical/java/impl/views/readme.md
@@ -0,0 +1,2 @@
+#Views
+This directory contains the views (i.e. FXML) for the GUI.
