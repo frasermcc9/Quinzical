2cda42e7d1676a2a4222274a8106179f9f931680
Fraser McCallum <fraser@themccallums.net.nz>
2020-09-19 22:20:28 +1200

    Added event emitter to scene handler and added delegate methods

--- src/main/java/quinzical/impl/models/SceneHandlerImpl.java
+++ src/main/java/quinzical/impl/models/SceneHandlerImpl.java
@@ -4,13 +4,19 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import quinzical.impl.constants.GameEvent;
 import quinzical.impl.constants.GameScene;
+import quinzical.interfaces.events.EventEmitter;
+import quinzical.interfaces.events.EventFunction;
 import quinzical.interfaces.models.SceneHandler;
 import quinzical.interfaces.models.SceneRegistry;
 
 @Singleton
 public class SceneHandlerImpl implements SceneHandler {
     private final Stage stage;
+
+    @Inject
+    EventEmitter emitter;
 
     @Inject
     SceneRegistry sceneRegistry;
@@ -24,5 +30,15 @@ public class SceneHandlerImpl implements SceneHandler {
     public void setActiveScene(GameScene scene) {
         Scene s = sceneRegistry.getScene(scene);
         stage.setScene(s);
+    }
+
+    @Override
+    public EventEmitter on(GameEvent event, EventFunction cb) {
+        return emitter.on(event, cb);
+    }
+
+    @Override
+    public boolean emit(GameEvent event) {
+        return emitter.emit(event);
     }
 }
--- src/main/java/quinzical/interfaces/models/SceneHandler.java
+++ src/main/java/quinzical/interfaces/models/SceneHandler.java
@@ -1,7 +1,14 @@
 package quinzical.interfaces.models;
 
+import quinzical.impl.constants.GameEvent;
 import quinzical.impl.constants.GameScene;
+import quinzical.interfaces.events.EventEmitter;
+import quinzical.interfaces.events.EventFunction;
 
 public interface SceneHandler {
-    void setActiveScene(GameScene scene);
+    abstract void setActiveScene(GameScene scene);
+
+    EventEmitter on(GameEvent event, EventFunction cb);
+
+    boolean emit(GameEvent event);
 }
