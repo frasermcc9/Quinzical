6ca3edf70fd76b96f01180b6220d4253762474b3
Fraser McCallum <fraser@themccallums.net.nz>
2020-09-25 04:19:18 +1200

    Moved board loading logic to controller

--- src/main/java/quinzical/impl/controllers/GameController.java
+++ src/main/java/quinzical/impl/controllers/GameController.java
@@ -1,40 +1,40 @@
 package quinzical.impl.controllers;
 
 import com.google.inject.Inject;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import quinzical.impl.constants.GameEvent;
+import quinzical.Entry;
 import quinzical.impl.constants.GameScene;
+import quinzical.impl.models.structures.GameQuestion;
 import quinzical.interfaces.models.GameModel;
 import quinzical.interfaces.models.SceneHandler;
 import quinzical.interfaces.strategies.boardloader.BoardLoaderStrategyFactory;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 /**
- * Controller class for the Game view. This is the view that contains the
- * question board.
+ * Controller class for the Game view. This is the view that contains the question board.
  */
 public class GameController extends PrimarySceneController {
 
+    private final List<Button> buttons = new ArrayList<>();
     @Inject
     private GameModel gameModel;
     @Inject
     private SceneHandler sceneHandler;
     @Inject
     private BoardLoaderStrategyFactory boardLoaderStrategyFactory;
-
     @FXML
     private ResourceBundle resources;
     @FXML
     private URL location;
-
     @FXML
     private AnchorPane background;
     @FXML
@@ -46,18 +46,9 @@ public class GameController extends PrimarySceneController {
     @FXML
     private Pane paneHeader;
     @FXML
-    private Pane paneContent;
-
-    /**
-     * Fires when the menu button is clicked. Changes the active scene back to the
-     * intro scene.
-     *
-     * @param event
-     */
+    private ImageView imgBackground;
     @FXML
-    void btnMenuClick(ActionEvent event) {
-        sceneHandler.setActiveScene(GameScene.INTRO);
-    }
+    private ImageView imgOverlay;
 
     /**
      * Called when the FXML is finished loading.
@@ -65,6 +56,12 @@ public class GameController extends PrimarySceneController {
     @FXML
     void initialize() {
         assert btnMenu != null : "fx:id=\"btnMenu\" was not injected: check your FXML file 'game.fxml'.";
+
+        for (Node b : paneHeader.getChildren()) {
+            if (b instanceof Button) {
+                buttons.add((Button) b);
+            }
+        }
 
         listen();
     }
@@ -74,33 +71,70 @@ public class GameController extends PrimarySceneController {
      */
     private void listen() {
         // Listen for theme changes.
-        sceneHandler.on(GameEvent.LIGHT_THEME_ENABLED, () -> setTheme(Theme.LIGHT));
-        sceneHandler.on(GameEvent.DARK_THEME_ENABLED, () -> setTheme(Theme.DARK));
-
-        // Listen for when the board needs to be fully refreshed
-        sceneHandler.on(GameEvent.FULL_BOARD_REFRESH, () -> {
-            // Regenerate questions
-            gameModel.generateNewGameQuestionSet();
-            // Creates the layout of all the question buttons and headers.
-            boardLoaderStrategyFactory.createStrategy().injectComponents(paneHeader, paneContent).loadBoard();
-        });
+        sceneHandler.onBackgroundChange(img -> this.imgBackground.setImage(img));
+        gameModel.onQuestionsUpdate(this::refreshBoard);
     }
 
-    /**
-     * Implements abstract method. Returns the scene background for theme
-     * changing.
-     */
-    @Override
-    protected AnchorPane getBackground() {
-        return this.background;
+    private void refreshBoard() {
+
+        btnMenu.setOnAction(event -> sceneHandler.setActiveScene(GameScene.INTRO));
+
+        setOverlay(Overlay.CATEGORY);
+
+        Map<String, List<GameQuestion>> model = gameModel.getBoardQuestions();
+        List<String> keys = new ArrayList<>(model.keySet());
+
+        for (int i = 0; i < buttons.size(); i++) {
+            Button btn = buttons.get(i);
+
+            String category = keys.get(i);
+            int answered = model.get(category).stream().reduce(0, (sub, el) -> sub + (el.isAnswered() ? 1 : 0),
+                Integer::sum);
+
+            btn.setText(category + " - " + answered + "/5");
+            btn.setDisable(false);
+            btn.setOnAction((event) -> loadQuestions(model.get(category)));
+        }
     }
 
-    /**
-     * Overrides virtual method (optional hook).
-     */
-    @Override
-    public void setLabelTextColour(String colourHex) {
-        labelEarnings.setTextFill(Color.web(colourHex));
-        labelEarningsVal.setTextFill(Color.web(colourHex));
+    private void loadQuestions(List<GameQuestion> questions) {
+
+        btnMenu.setOnAction(event -> refreshBoard());
+
+        setOverlay(Overlay.QUESTION);
+
+        for (int i = 0; i < buttons.size(); i++) {
+            Button btn = buttons.get(i);
+            GameQuestion q = questions.get(i);
+
+            btn.setText(q.getValue() + "");
+            btn.setDisable(!q.isAnswerable());
+            btn.setOnAction((event) -> activateQuestion(q));
+        }
     }
+
+    private void activateQuestion(GameQuestion gameQuestion) {
+        gameModel.activateQuestion(gameQuestion);
+        sceneHandler.setActiveScene(GameScene.GAME_QUESTION);
+    }
+
+    private void setOverlay(Overlay o) {
+        String img = "";
+        switch (o) {
+            case CATEGORY:
+                img = "game";
+                break;
+            case QUESTION:
+                img = "game2";
+                break;
+
+        }
+        imgOverlay.setImage(new Image(Objects.requireNonNull(Entry.class.getClassLoader().getResourceAsStream("images" +
+            "/overlays/" + img + ".png"))));
+    }
+
+    private enum Overlay {
+        CATEGORY, QUESTION
+    }
+
 }
