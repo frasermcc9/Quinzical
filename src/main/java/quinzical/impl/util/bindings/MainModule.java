62e3c0afa97cd964dc04402cd92e6d1da6dc45a9
Fraser McCallum <fraser@themccallums.net.nz>
2020-09-27 18:44:12 +1300

    Added bindings for normaliser strategy and question verifier. Changed speaker bindings to give a different class on windows.

--- src/main/java/quinzical/impl/util/bindings/MainModule.java
+++ src/main/java/quinzical/impl/util/bindings/MainModule.java
@@ -7,9 +7,12 @@ import quinzical.impl.models.QuestionCollectionImpl;
 import quinzical.impl.models.SceneHandlerImpl;
 import quinzical.impl.models.SceneRegistryImpl;
 import quinzical.impl.models.structures.SpeakerManager;
+import quinzical.impl.models.structures.WindowsSpeakerManager;
 import quinzical.impl.util.factories.BoardComponentFactoryImpl;
 import quinzical.impl.util.strategies.boardloader.BoardLoaderStrategyFactoryImpl;
 import quinzical.impl.util.strategies.questiongenerator.QuestionGeneratorStrategyFactoryImpl;
+import quinzical.impl.util.strategies.questionverifier.QuestionVerifierFactoryImpl;
+import quinzical.impl.util.strategies.textnormaliser.TextNormaliserFactoryImpl;
 import quinzical.interfaces.factories.BoardComponentFactory;
 import quinzical.interfaces.models.GameModel;
 import quinzical.interfaces.models.QuestionCollection;
@@ -18,6 +21,8 @@ import quinzical.interfaces.models.SceneRegistry;
 import quinzical.interfaces.models.structures.Speaker;
 import quinzical.interfaces.strategies.boardloader.BoardLoaderStrategyFactory;
 import quinzical.interfaces.strategies.questiongenerator.QuestionGeneratorStrategyFactory;
+import quinzical.interfaces.strategies.questionverifier.QuestionVerifierFactory;
+import quinzical.interfaces.strategies.textnormaliser.TextNormaliserFactory;
 
 /**
  * This class binds concrete implementations to interfaces. It decides, when a class requires an interface injection
@@ -43,7 +48,15 @@ public class MainModule extends AbstractModule {
         bind(BoardLoaderStrategyFactory.class).to(BoardLoaderStrategyFactoryImpl.class);
         bind(BoardComponentFactory.class).to(BoardComponentFactoryImpl.class);
 
-        bind(Speaker.class).to(SpeakerManager.class);
+        bind(TextNormaliserFactory.class).to(TextNormaliserFactoryImpl.class);
+        bind(QuestionVerifierFactory.class).to(QuestionVerifierFactoryImpl.class);
+
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            bind(Speaker.class).to(WindowsSpeakerManager.class);
+        } else {
+            bind(Speaker.class).to(SpeakerManager.class);
+        }
+
 
         bind(Stage.class).toInstance(stage);
     }
