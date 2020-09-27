62041b0721df15f39d6b698e1b254e020993b2d8
Fraser McCallum <fraser@themccallums.net.nz>
2020-09-27 18:43:40 +1300

    Added a speaker manager for when running on windows

--- src/main/java/quinzical/impl/models/structures/WindowsSpeakerManager.java
+++ src/main/java/quinzical/impl/models/structures/WindowsSpeakerManager.java
@@ -1,2 +1,10 @@
-package quinzical.impl.models.structures;public class WindowsSpeakerManager {
+package quinzical.impl.models.structures;
+
+import quinzical.interfaces.models.structures.Speaker;
+
+public class WindowsSpeakerManager implements Speaker {
+    @Override
+    public void speak(String text) {
+        System.out.println("Operating System is windows. Printing TTS: " + text);
+    }
 }
