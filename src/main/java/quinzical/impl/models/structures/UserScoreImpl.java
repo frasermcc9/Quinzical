58a4fc5a79b0e1f995d0ef22bc899e97e7f9b80d
Fraser McCallum <fraser@themccallums.net.nz>
2020-09-30 03:06:50 +1300

    Added javadoc

--- src/main/java/quinzical/impl/models/structures/UserScoreImpl.java
+++ src/main/java/quinzical/impl/models/structures/UserScoreImpl.java
@@ -1,21 +1,37 @@
 package quinzical.impl.models.structures;
 
 import com.google.inject.Inject;
-import quinzical.interfaces.models.structures.UserScore;
 import quinzical.interfaces.models.GameModel;
+import quinzical.interfaces.models.structures.UserScore;
 
 public class UserScoreImpl implements UserScore {
 
+    /**
+     * GameModel singleton to alert value changes to
+     */
     @Inject
     private GameModel gameModel;
 
+    /**
+     * The users earnings
+     */
     private int value = 0;
 
-    @Override public int getValue() {
+    /**
+     * @return gets the user's current earnings
+     */
+    @Override
+    public int getValue() {
         return value;
     }
 
-    @Override public void setValue(int value) {
+    /**
+     * Sets the value of the users earnings. Fires the value change event to the GameModel.
+     *
+     * @param value the value to set the earnings to.
+     */
+    @Override
+    public void setValue(int value) {
         this.value = value;
         gameModel.fireValueChange();
     }
