fa04666bef56687eaa86be8042ef3da2194894a3
Fraser McCallum <fraser@themccallums.net.nz>
2020-09-30 18:35:17 +1300

    Changed method signature to argumentless method

--- src/main/java/quinzical/interfaces/events/ActiveQuestionObserver.java
+++ src/main/java/quinzical/interfaces/events/ActiveQuestionObserver.java
@@ -1,7 +1,5 @@
 package quinzical.interfaces.events;
 
-import quinzical.impl.models.structures.GameQuestion;
-
 public interface ActiveQuestionObserver {
-    void fireActiveQuestion(GameQuestion gq);
+    void fireActiveQuestion();
 }
