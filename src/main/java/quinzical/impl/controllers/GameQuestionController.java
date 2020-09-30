81ebc0515a16d73da4f851736b646fd726184ed7
Fraser McCallum <fraser@themccallums.net.nz>
2020-09-30 18:33:14 +1300

    Added support for adding macrons to the text. Organised code and added documentation.

--- src/main/java/quinzical/impl/controllers/GameQuestionController.java
+++ src/main/java/quinzical/impl/controllers/GameQuestionController.java
@@ -4,12 +4,12 @@ import com.google.inject.Inject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import quinzical.Entry;
 import quinzical.impl.constants.GameScene;
@@ -26,6 +26,8 @@ import java.util.Objects;
 
 public class GameQuestionController {
 
+    //#region Injected classes
+
     @Inject
     private SceneHandler sceneHandler;
 
@@ -38,14 +40,12 @@ public class GameQuestionController {
     @Inject
     private QuestionVerifierFactory questionVerifierFactory;
 
-    @FXML
-    private AnchorPane background;
+    //#endregion
+
+    //#region Injected FXML components
 
     @FXML
     private ImageView imgBackground;
-
-    @FXML
-    private ImageView imgOverlay;
 
     @FXML
     private Pane paneSolutions;
@@ -59,67 +59,61 @@ public class GameQuestionController {
     @FXML
     private Button btnPass;
 
+    @FXML
+    private ButtonBar macronBar;
+
+    //#endregion
+
     private List<TextArea> textAreas;
+
+    private TextArea activeText = null;
+
+    //#region Initialisation at FXML load
 
     @FXML
     void initialize() {
         listen();
+        initMacronButtons();
     }
 
     private void listen() {
-        sceneHandler.onBackgroundChange(img -> this.imgBackground.setImage(img));
+        //Listens for theme change
+        sceneHandler.onBackgroundChange(this.imgBackground::setImage);
+        //Listens for when a new active question is activated
         gameModel.onActiveQuestionUpdate(this::initialiseQuestion);
     }
+
+    /**
+     * This sets up the macron buttons
+     */
+    private void initMacronButtons() {
+        //gets each button in the bar, and programs its handler
+        macronBar.getButtons().forEach(b -> {
+            if (b instanceof Button) {
+                Button btn = (Button) b;
+                // Gets the text and current cursor location from the selected text area and inserts the selected 
+                // macron character after the cursor. Then reselect the text area and set the cursor to after the 
+                // inserted character.
+                btn.setOnAction(e -> {
+                    String text = activeText.getText();
+                    int currentPos = activeText.getCaretPosition();
+                    String newText = text.substring(0, currentPos) + btn.getText() + text.substring(currentPos);
+                    activeText.setText(newText);
+                    activeText.requestFocus();
+                    activeText.positionCaret(currentPos + 1);
+                });
+            }
+        });
+    }
+
+    //#endregion
+
+    //#region Injected handlers
 
     @FXML
     void onReplyClick() {
         String s = gameModel.getActiveQuestion().getHint();
         speaker.speak(s);
-    }
-
-    private void initialiseQuestion(GameQuestion gameQuestion) {
-
-        gameModel.answerActive(false);
-
-        textAreas = new ArrayList<>();
-        paneSolutions.getChildren().clear();
-
-        this.lblPrompt.setText(gameQuestion.getPrompt() + ":");
-
-        speaker.speak(gameQuestion.getHint());
-
-        int slnSize = gameQuestion.getSolutions().size();
-
-        for (int i = 0; i < slnSize; i++) {
-            TextArea ta = new TextArea();
-            ta.getStylesheets().add(Objects.requireNonNull(Entry.class.getClassLoader().getResource("css/game" +
-                "-question.css")).toExternalForm());
-            ta.applyCss();
-            ta.setPrefWidth(paneSolutions.getPrefWidth());
-            ta.setMaxHeight(paneSolutions.getPrefHeight() / slnSize - slnSize);
-            ta.setLayoutY(0 + i * paneSolutions.getPrefHeight() / slnSize + 1);
-            ta.setPromptText("Enter your solution here...");
-            ta.setOnKeyPressed(this::onEnterPressed);
-            paneSolutions.getChildren().add(ta);
-            textAreas.add(ta);
-        }
-        textAreas.get(0).requestFocus();
-    }
-
-    private void onEnterPressed(KeyEvent e) {
-        if (e.getCode() == KeyCode.ENTER) {
-            if (e.getSource() instanceof TextArea) {
-                TextArea ta = (TextArea) e.getSource();
-                ta.setText(ta.getText().trim());
-                int idx = textAreas.indexOf(ta);
-                if (idx + 1 == textAreas.size()) {
-                    btnSubmit.fire();
-                } else {
-                    textAreas.get(idx + 1).requestFocus();
-                }
-            }
-
-        }
     }
 
     @FXML
@@ -144,22 +138,63 @@ public class GameQuestionController {
 
     }
 
+    //#endregion
+
+    /**
+     * Runs whenever a new active question is set.
+     */
+    private void initialiseQuestion() {
+
+        GameQuestion gameQuestion = gameModel.getActiveQuestion();
+
+        // As soon as the question is 
+        gameModel.answerActive(false);
+
+        // Clear the text area list and the pane of any existing text areas from previous questions.
+        textAreas = new ArrayList<>();
+        paneSolutions.getChildren().clear();
+
+        //set the prompt text (i.e. what is...)
+        this.lblPrompt.setText(gameQuestion.getPrompt() + ":");
+
+        //speak the question
+        speaker.speak(gameQuestion.getHint());
+
+        // Generate a text area for each solution.
+        int slnSize = gameQuestion.getSolutions().size();
+        for (int i = 0; i < slnSize; i++) {
+            TextArea ta = new TextArea();
+            // Style the text area
+            ta.getStylesheets().add(Objects.requireNonNull(Entry.class.getClassLoader().getResource("css/game" +
+                "-question.css")).toExternalForm());
+            ta.applyCss();
+            // Apply layout to the text area so it fits in the bounding box
+            ta.setPrefWidth(paneSolutions.getPrefWidth());
+            ta.setMaxHeight(paneSolutions.getPrefHeight() / slnSize - slnSize);
+            ta.setLayoutY(0 + i * paneSolutions.getPrefHeight() / slnSize + 1);
+
+            // Listen for when a key is pressed in the text area. This function listens for the enter key as a 
+            // keyboard shortcut.
+            ta.setOnKeyPressed(this::onTextAreaKeyPress);
+
+            // Listen for when the text area is focused. When it is focused, set activeText to this text area. This is
+            // for knowing which text area to insert the macron characters into when the buttons are chosen.
+            ta.focusedProperty().addListener((_l, _o, isFocused) -> activeText = isFocused ? ta : activeText);
+
+            //Add the text area to the pane and to the list of text areas.
+            paneSolutions.getChildren().add(ta);
+            textAreas.add(ta);
+        }
+        textAreas.get(0).requestFocus();
+    }
+
     private void handleReturnToCategories(ActionEvent e) {
-        btnSubmit.setOnAction(_e -> onSubmitClicked());
-        btnPass.setOnAction(_e -> onSubmitClicked());
-        btnPass.setText("Pass");
-        btnSubmit.setText("Submit");
-
+        refreshSubmissionButtonsState();
         sceneHandler.setActiveScene(GameScene.GAME);
-
     }
 
     private void handleNextQuestion(GameQuestion question) {
-        btnSubmit.setOnAction(_e -> onSubmitClicked());
-        btnPass.setOnAction(_e -> onSubmitClicked());
-        btnPass.setText("Pass");
-        btnSubmit.setText("Submit");
-
+        refreshSubmissionButtonsState();
         GameQuestion next = gameModel.getNextActiveQuestion(question);
         if (next == null) {
             sceneHandler.setActiveScene(GameScene.GAME);
@@ -169,5 +204,28 @@ public class GameQuestionController {
 
     }
 
+    private void refreshSubmissionButtonsState() {
+        btnSubmit.setOnAction(e -> onSubmitClicked());
+        btnPass.setOnAction(e -> onSubmitClicked());
+        btnPass.setText("Pass");
+        btnSubmit.setText("Submit");
+    }
+
+    private void onTextAreaKeyPress(KeyEvent e) {
+        if (e.getCode() == KeyCode.ENTER) {
+            if (e.getSource() instanceof TextArea) {
+                TextArea ta = (TextArea) e.getSource();
+                ta.setText(ta.getText().trim());
+                int idx = textAreas.indexOf(ta);
+                if (idx + 1 == textAreas.size()) {
+                    btnSubmit.fire();
+                    btnPass.requestFocus();
+                } else {
+                    textAreas.get(idx + 1).requestFocus();
+                }
+            }
+
+        }
+    }
 
 }
