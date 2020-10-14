c7335841dda57ccc81e126dca5c47b478eae501b
Fraser McCallum <fraser@themccallums.net.nz>
2020-10-15 00:40:45 +1300

    Major view refactoring Wrapped most components in outer vbox Top level components are still anchor pane - outside of scene Changed overlays to be background image of vbox Text from overlay is now supplied from labels

---
+++ src/main/java/quinzical/impl/controllers/StandardSceneController.java
@@ -0,0 +1,34 @@
+// Copyright 2020 Fraser McCallum and Braden Palmer
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package quinzical.impl.controllers;
+
+import com.google.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import quinzical.interfaces.models.SceneHandler;
+
+public abstract class StandardSceneController {
+
+    @FXML
+    protected VBox background;
+
+    @Inject
+    protected SceneHandler sceneHandler;
+
+    @FXML
+    final void initialize() {
+        this.sceneHandler.onBackgroundChange(image -> background.setStyle("-fx-background-image:" + image));
+    }
+}
