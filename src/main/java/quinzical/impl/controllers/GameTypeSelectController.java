91a5c96e8809826bf1b95215af5a1ea029e94244
bpal938 <bradenpalmer2000@gmail.com>
2020-10-15 14:59:20 +1300

    added gametypeselect fxml and controller file/class

---
+++ src/main/java/quinzical/impl/controllers/GameTypeSelectController.java
@@ -0,0 +1,19 @@
+package quinzical.impl.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class GameTypeSelectController {
+    
+    @FXML
+    void btnInternationalPress(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    void btnNZPress(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    void btnBackPress(ActionEvent actionEvent) {
+    }
+}
