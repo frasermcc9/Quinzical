dde6f4824f6bd3e40f6e9ef4cbaeebdcda123464
Fraser McCallum <fraser@themccallums.net.nz>
2020-10-21 20:55:46 +1300

    Reworked scene. Added practice mode to this scene. Changed to material theme.

--- src/main/java/quinzical/impl/controllers/GameTypeSelectController.java
+++ src/main/java/quinzical/impl/controllers/GameTypeSelectController.java
@@ -1,57 +1,41 @@
 package quinzical.impl.controllers;
 
 import com.google.inject.Inject;
+import com.jfoenix.controls.JFXSpinner;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ProgressIndicator;
+import javafx.scene.effect.Glow;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import quinzical.impl.constants.GameScene;
 import quinzical.interfaces.models.GameModel;
 import quinzical.interfaces.models.SceneHandler;
 
-public class GameTypeSelectController extends StandardSceneController {
+public class GameTypeSelectController extends AbstractSceneController {
+
+    private static final Runnable[] buttonActions = new Runnable[3];
 
     @Inject
     private SceneHandler sceneHandler;
-
     @Inject
     private GameModel gameModel;
 
     @FXML
-    private ProgressIndicator progressIndicator;
-
+    private JFXSpinner progressIndicator;
     @FXML
     private Label lblProgress;
-
     @FXML
     private Button btnInternational;
-
     @FXML
-    void btnInternationalPress() {
-        setProgressVisible(true);
-        Task<Void> task = new Task<>() {
-            @Override
-            protected Void call() {
-                gameModel.generateInternationalQuestions();
-                return null;
-            }
-
-            @Override
-            protected void succeeded() {
-                Platform.runLater(() -> sceneHandler.setActiveScene(GameScene.INTERNATIONAL_GAME));
-            }
-        };
-        Thread thread = new Thread(task);
-        thread.setDaemon(true);
-        thread.start();
-    }
-
+    private HBox imageContainer;
     @FXML
-    void btnNZPress() {
-        sceneHandler.setActiveScene(GameScene.CATEGORY_SELECTOR);
-    }
+    private Button btnPlay;
+    @FXML
+    private Label lblInternational;
 
     @FXML
     void btnBackPress() {
@@ -66,8 +50,68 @@ public class GameTypeSelectController extends StandardSceneController {
     @Override
     protected void onLoad() {
         setProgressVisible(false);
-        // Disable the international button if it is not unlocked
-        btnInternational.setDisable(!gameModel.isInternationalUnlocked());
+        btnPlay.setDisable(true);
+        setFunctions();
 
+        Glow glow = new Glow(100);
+
+        final int startPoint = 0;
+        final int endPoint;
+
+        if (!gameModel.isInternationalUnlocked()) {
+            endPoint = 2;
+            StackPane international = (StackPane) imageContainer.getChildren().get(2);
+            createScaleAnimation(international, 100, 0.9);
+            adjustBrightness(international.getChildren().get(0), 100, -0.8);
+        } else {
+            lblInternational.setText("International Questions");
+            endPoint = 3;
+        }
+
+        for (int i = startPoint; i < endPoint; i++) {
+            final int idx = i;
+            Node image = imageContainer.getChildren().get(idx);
+            image.setOnMouseEntered(e -> image.setEffect(glow));
+            image.setOnMouseExited(e -> image.setEffect(null));
+            image.setOnMouseClicked(e -> disableImagesExcept(idx, startPoint, endPoint));
+        }
+    }
+
+    private void disableImagesExcept(int i, int start, int end) {
+        for (int j = start; j < end; j++) {
+            Node image = imageContainer.getChildren().get(j);
+            if (j == i) {
+                createScaleAnimation(image, 400, 1.1);
+                btnPlay.setDisable(false);
+                btnPlay.setOnAction(a -> buttonActions[i].run());
+            } else {
+                createScaleAnimation(image, 400, 0.9);
+            }
+        }
+    }
+
+    private void setFunctions() {
+        buttonActions[0] = () -> sceneHandler.setActiveScene(GameScene.PRACTICE);
+        buttonActions[1] = () -> sceneHandler.setActiveScene(GameScene.CATEGORY_SELECTOR);
+        buttonActions[2] = this::loadInternational;
+    }
+
+    private void loadInternational() {
+        setProgressVisible(true);
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() {
+                gameModel.generateInternationalQuestions();
+                return null;
+            }
+
+            @Override
+            protected void succeeded() {
+                Platform.runLater(() -> sceneHandler.setActiveScene(GameScene.GAME));
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
     }
 }
