405a2baa13d1d4fbc455877152a141fe390312bf
Fraser McCallum <fraser@themccallums.net.nz>
2020-10-31 22:11:42 +1300

    Changed filechooser to native version. Cleaned up try catches.

--- src/main/java/quinzical/impl/util/questionparser/Serializer.java
+++ src/main/java/quinzical/impl/util/questionparser/Serializer.java
@@ -14,79 +14,86 @@
 
 package quinzical.impl.util.questionparser;
 
-import javax.swing.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.FileChooser;
+
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * Allows the user to locate a file within their file system containing correctly formatted questions,
- * and then reads the files, creating a map containing the questions in it
+ * Allows the user to locate a file within their file system containing correctly formatted questions, and then reads
+ * the files, creating a map containing the questions in it
  */
 public class Serializer {
 
     public static void main(String[] args) {
 
-        File file;
+        //JFileChooser fileChooser = new JFileChooser();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+        File file = fileChooser.showOpenDialog(null);
 
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
-        int result = fileChooser.showOpenDialog(null);
-        if (result == JFileChooser.APPROVE_OPTION) {
-            file = fileChooser.getSelectedFile();
-        } else {
-            return;
-        }
+        if (file == null) return;
 
-        Map<String, List<Question>> M = new HashMap<>();
-        String activeCategory = "";
-
-        List<String> lines = Collections.emptyList();
         try {
+            Map<String, List<Question>> M = new HashMap<>();
+            String activeCategory = "";
+
+            List<String> lines;
+
             lines = Files.readAllLines(Paths.get(file.getPath()), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        for (String line : lines) {
-            line = line.trim();
 
-            if (!line.matches("^.*\\|.*\\|.*$")) {
-                activeCategory = line;
-                M.put(activeCategory, new ArrayList<>());
-                continue;
+            for (String line : lines) {
+                line = line.trim();
+                if (!line.matches("^.*\\|.*\\|.*$")) {
+                    activeCategory = line;
+                    M.put(activeCategory, new ArrayList<>());
+                    continue;
+                }
+                String[] parts = line.split("\\|");
+                String hint = parts[0].trim();
+                String prompt = parts[1].trim();
+
+                Question q = new Question(activeCategory, hint, prompt);
+
+                String solutions = parts[2].trim();
+
+                String[] slnList = solutions.split(",");
+                for (String sln : slnList) {
+                    String[] variants = sln.split("/");
+                    q.addSolution(variants);
+                }
+
+                M.get(activeCategory).add(q);
+
             }
-            String[] parts = line.split("\\|");
-            String hint = parts[0].trim();
-            String prompt = parts[1].trim();
 
-            Question q = new Question(activeCategory, hint, prompt);
-
-            String solutions = parts[2].trim();
-
-            String[] slnList = solutions.split(",");
-            for (String sln : slnList) {
-                String[] variants = sln.split("/");
-                q.addSolution(variants);
-            }
-
-            M.get(activeCategory).add(q);
-
-        }
-
-        try {
-            FileOutputStream fileOut = new FileOutputStream(file.getParent() + "/question2.qdb");
+            FileOutputStream fileOut = new FileOutputStream(System.getProperty("user.dir") + "/data/question.qdb");
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             out.writeObject(M);
             out.close();
             fileOut.close();
-        } catch (IOException i) {
-            i.printStackTrace();
+        } catch (Exception ex) {
+            showAlert();
         }
+    }
+
+    private static void showAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR, "Please check the file and try again.",
+            ButtonType.OK, ButtonType.CANCEL);
+        alert.setTitle("Invalid Question Set Loaded");
+        alert.setHeaderText("This file does not appear to be a valid Quinzical Data Format.");
+        alert.showAndWait();
     }
 }
