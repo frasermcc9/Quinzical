c7e4c56f81ec64df3b5cccc95966fc3ed7845c6b
Fraser McCallum <fraser@themccallums.net.nz>
2020-10-31 23:15:53 +1300

    Changed server to submodule

--- src/main/server/src/main.ts
+++
@@ -1,8 +0,0 @@
-import {EventEmitter} from "events";
-import {decorate, injectable} from "inversify";
-import {IoCContainer} from "./bindings/inversify.config";
-import {TYPES} from "./bindings/types";
-import {SocketManager} from "./server/Socket";
-
-decorate(injectable(), EventEmitter);
-const socketManager = IoCContainer.get<SocketManager>(TYPES.SocketManager);
